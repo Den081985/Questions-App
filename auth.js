fabc1c3ad9fb7e91224d838dd72cc3191a10b254
Den081985 <pishnenkod@gmail.com>
2022-01-16 18:48:36 +0300

    добавлено authWithEmailAndPassword

--- auth.js
+++ auth.js
@@ -20,6 +20,7 @@ export function getAuthHtml() {
       <label for="password">Пароль:</label>
     </d>
     <button
+      id="submit-btn"
       type="submit"
       class="mui-btn mui-btn--raised mui-btn--primary"
       >
@@ -28,4 +29,25 @@ export function getAuthHtml() {
   </form>
     
     `;
+}
+//функция для авторизации пользователя с email и паролем
+//запрос на auth REST API firebase с API_KEY
+export function authWithEmailAndPassword(email, password) {
+  const apiKey = "AIzaSyD4yWr-5pKa1zc8sM_GaQEyz4UpkqnD57g";
+  return fetch(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${apiKey}`,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => data.idToken); //idToken получаем с ответом от сервера
 }
